9ecfb8cfd8ea4d7e942f41bdac9b5e56c1d9c5d0
angely.gm03 <gmartinez.angely@gmail.com>
2024-03-07 09:36:08 -0600

    pantalla de inicio

--- frontend/src/components/Inicio/Inicio.js
+++ frontend/src/components/Inicio/Inicio.js
@@ -1,18 +1,49 @@
 import styles from './Inicio.module.css';
-import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
+import React,{useState} from "react";
+import { useNavigate} from 'react-router-dom';
 
-function Inicio() {
-    const [cursos, setCursos] = useState([]);
-
+function Inicio(){
+    const nav = useNavigate
+    const handleAcpAlert =()=>{
+        nav('./login');
+    }
     return (
-        <div className={styles.formContainer}>
-        <h2 className={styles.topText}>Universidad de San Carlos de Guatemala</h2>
-        <h3 className={styles.topText}>Facultad de Ingeniería</h3>
-        <h4 className={styles.subtitle}>-Cursos Primer Semestre 2024-</h4>
-       </div>     
+        <body class={styles.appwrapper}>
+            <div class={styles.botonperfil}>
+                <button>Ver Perfil</button>
+            </div>
+            <div className={styles.imagencontainer}>
+                <img src='../assets/logouazul.jpg' alt="LogoUsac" className={styles.imagen}/>
+            </div>
+            <div class={styles.buscador}>
+                <span class={styles.buscador.lupa}>&#128269;</span>
+                <input type="text" placeholder="Buscar Usuario"></input>
+                <button>Buscar</button>
+            </div>
+            <textarea
+            className={styles.textar}
+            value='Name User'
+            readOnly 
+            />
+            <div class={styles.contenedor}>
+                <div class={styles.botonpubli}>
+                    <button>Crear Publicacion</button>
+                </div>
+                <div class={styles.cuadrado}></div>
+                <label class={styles.label}>Publicaciones</label>
+                <div class={styles.listafiltros}>
+                    <h3>Filtros</h3>
+                        <ul>
+                            <li><input type="radio" id="filtro1" name= "filtro"></input><label for="filtro1">Fecha de Creacion</label></li>
+                            <li><input type="radio" id="filtro1" name= "filtro"></input><label for="filtro1">Filtrar por Curso</label></li>
+                            <li><input type="radio" id="filtro2" name= "filtro"></input><label for="filtro2">Filtrar por Catedratico</label></li>
+                            <li><input type="radio" id="filtro3" name= "filtro"></input><label for="filtro3">Filtro por Nombre de Curso</label></li>
+                            <li><input type="radio" id="filtro3" name= "filtro"></input><label for="filtro3">Filtrar Por Nombre de Catedratico</label></li>
+                        </ul>
+                </div>
+            </div>
+        </body>
     );
 }
+
 export default Inicio;
-
-
